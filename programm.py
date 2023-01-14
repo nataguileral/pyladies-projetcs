d9f3a1da2c1e153d21335b699b1cb963770faf84
Mateusz Podleśny <17722512+m-podlesny@users.noreply.github.com>
2023-01-14 18:47:17 +0100

    Add get_cookies function.

--- programm.py
+++ programm.py
@@ -1,4 +1,9 @@
 def print_info(number: int):
     print(input("What to print?" + str(number)))
 
+
 print_info()
+
+
+def get_cookies(number: int):
+    return print("you have " + number + "cookies")
