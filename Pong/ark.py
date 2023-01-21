32e82229f9225fc216f44a2092258df9f325a627
Mateusz Podleśny <17722512+m-podlesny@users.noreply.github.com>
2023-01-21 16:30:45 +0100

    change function name to head...

--- Pong/ark.py
+++ Pong/ark.py
@@ -1,4 +1,4 @@
-def main(list_orders: list[str]):
+def make_hedgehog_invasion(list_orders: list[str]):
 
     list_n: list[int] = []
 
@@ -22,7 +22,7 @@ def main(list_orders: list[str]):
             print(list_n)
 
 
-main(list(['insert 0 5',
+make_hedgehog_invasion(list(['insert 0 5',
            'insert 1 10',
            'insert 0 6',
            'print',
